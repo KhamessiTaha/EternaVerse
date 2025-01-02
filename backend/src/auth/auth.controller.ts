f32c019ac30ac822da6a5baeaaf221b52d01bb4e
Taha Khamessi <taha.khamessi@gmail.com>
2025-01-03 00:26:40 +0100

    Auth + JWT

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Request } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  async register(@Body() user: any) {
+    return this.authService.register(user);
+  }
+
+  @Post('login')
+  async login(@Body() user: any) {
+    return this.authService.login(user);
+  }
+}
