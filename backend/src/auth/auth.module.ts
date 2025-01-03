fd459ee37e2493b52468d6224f237d8ec27b3a4d
Taha Khamessi <taha.khamessi@gmail.com>
2025-01-03 17:36:25 +0100

    Fixed the process.env.JWT_SECRET not found

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
@@ -9,6 +10,7 @@ import { AuthController } from './auth.controller';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     UsersModule,
     PassportModule,
     JwtModule.register({
@@ -20,5 +22,4 @@ import { AuthController } from './auth.controller';
   providers: [AuthService, LocalStrategy, JwtStrategy],
   exports: [AuthService],
 })
-
 export class AuthModule {}
